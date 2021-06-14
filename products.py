082c8423f16f627808384cfae78919a63f0cfe50
Larry Chen <abclarryabclarry@gmail.com>
2021-06-14 15:02:51 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,11 @@
+products = []
+while True:
+	 name = input('請輸入商品名稱: ')
+	 if name == 'q':
+	 	break
+	 price = input('請輸入商品價格: ')
+	 # 縮寫 p = [name, price]
+	 # 縮寫 p.append(name)
+	 # 縮寫 p.append(price) , p 為小清單
+	 products.append([name, price]) # products 為大清單
+print(products)
